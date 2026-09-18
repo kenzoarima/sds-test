54dccc85539e92f8d0104cb251c6898dbc480480
kenzoarima <suprabluesil@gmail.com>
2026-09-19 00:48:11 +0800

    update Button figma cc file to pick up the size variant

--- src/figma/primitives/button/Button.figma.ts
+++ src/figma/primitives/button/Button.figma.ts
@@ -21,6 +21,7 @@ const iconEnd = instance.getBoolean("Has Icon End", {
   true: iconEndSwap?.executeTemplate().example,
 });
 const size = instance.getEnum("Size", {
+  Medium: "medium",
   Small: "small",
 });
 const isDisabled = instance.getEnum("State", {
